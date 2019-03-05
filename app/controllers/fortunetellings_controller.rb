4e54a4c73c6fa943a4e33d11b1b033c4c12ec34c
Yamamotonasu <deep.blue.deep@outlook.jp>
2019-03-05 16:27:45 +0900

    fortunetellingsコントローラの作成と誕生日を表示

---
+++ app/controllers/fortunetellings_controller.rb
@@ -0,0 +1,9 @@
+class FortunetellingsController < ApplicationController
+  def show
+    @birthday = params[:birthday]
+    # 8桁で入力されたパラメータを参照して代入する
+    @year = @birthday[0, 4].to_i
+    @month = @birthday[4, 2].to_i
+    @day = @birthday[6, 2].to_i
+  end
+end
