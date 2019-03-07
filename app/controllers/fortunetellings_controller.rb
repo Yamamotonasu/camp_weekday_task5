f0aba0dc6f177b5eac248dae8d9754f33a084a11
Yamamotonasu <deep.blue.deep@outlook.jp>
2019-03-07 09:34:51 +0900

    リファクタリング

--- app/controllers/fortunetellings_controller.rb
+++ app/controllers/fortunetellings_controller.rb
@@ -4,9 +4,9 @@ class FortunetellingsController < ApplicationController
   def show
     @birthday = params[:birthday]
     # 8桁で入力されたパラメータを参照して代入する
-    @year = @birthday[0, 4].to_i
-    @month = @birthday[4, 2].to_i
-    @day = @birthday[6, 2].to_i
+    @year = @birthday[0, 4]
+    @month = @birthday[4, 2]
+    @day = @birthday[6, 2]
     @constellation = determine_constellation(@month, @day)
     @age = calculate_age(@birthday)
     @fortunes = %w(大吉 中吉 吉 小吉 凶 大凶).sample
--- app/helpers/fortunetellings_helper.rb
+++ app/helpers/fortunetellings_helper.rb
@@ -1,29 +1,31 @@
 module FortunetellingsHelper
   # 生まれた月と日を渡すと星座を返す
   def determine_constellation(month, day)
-    if month == 1 && day.between?(20, 31) || month == 2 && day.between?(1, 18)
+    days = (month + day).to_i
+
+    if days.between?(120, 218)
       '水瓶'
-    elsif month == 2 && day.between?(19, 29) || month == 3 && day.between?(1, 20)
+    elsif days.between?(219, 320)
       '魚'
-    elsif month == 3 && day.between?(21, 31) || month == 4 && day.between?(1, 19)
+    elsif days.between?(321, 419)
       '牡羊'
-    elsif month == 4 && day.between?(20, 30) || month == 5 && day.between?(1, 20)
+    elsif days.between?(420, 520)
       '牡牛'
-    elsif month == 5 && day.between?(21, 31) || month == 6 && day.between?(1, 21)
+    elsif days.between?(521, 621)
       '双子'
-    elsif month == 6 && day.between?(22, 30) || month == 7 && day.between?(1, 22)
+    elsif days.between?(622, 722)
       '蟹'
-    elsif month == 7 && day.between?(23, 31) || month == 8 && day.between?(1, 22)
+    elsif days.between?(723, 822)
       '獅子'
-    elsif month == 8 && day.between?(23, 31) || month == 9 && day.between?(1, 22)
+    elsif days.between?(823, 922)
       '乙女'
-    elsif month == 9 && day.between?(23, 30) || month == 10 && day.between?(1, 23)
+    elsif days.between?(923, 1023)
       '天秤'
-    elsif month == 10 && day.between?(24, 31) || month == 11 && day.between?(1, 22)
+    elsif days.between?(1024, 1122)
       '蠍'
-    elsif month == 11 && day.between?(23, 30) || month == 12 && day.between?(1, 21)
+    elsif days.between?(1123, 1221)
       '射手'
-    elsif month == 12 && day.between?(22, 31) || month == 1 && day.between?(1, 19)
+    elsif days.between?(1222, 1231) || days.between?(101, 119)
       '山羊'
     end
   end
@@ -34,12 +36,13 @@ module FortunetellingsHelper
   end
 
   # yyyymmdd形式で有効な生年月日ならtrueを返す
-  def valid_date?(birthday)
-    Date.valid_date?(@year, @month, @day) && @birthday.length == 8 && Date.today.strftime("%Y%m%d").to_i >= @birthday.to_i
+  # 存在する日付であること、入力された値が8桁である事、未来の日付は無効であること
+  def valid_birthday?(birthday)
+    Date.valid_date?(@year.to_i, @month.to_i, @day.to_i) && birthday.length == 8 && Date.today.strftime("%Y%m%d").to_i >= birthday.to_i
   end
 
   # 今日が誕生日ならtrueを返す
   def is_birthday?(birthday)
-    Date.today.strftime("%m%d").to_i == birthday[4, 4].to_i
+    Date.today.strftime("%m%d") == birthday[4, 4]
   end
 end
